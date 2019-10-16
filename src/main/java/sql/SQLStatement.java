2f6ff40694c601d2f9fe3844a6a17eb8d5d270c4
shahaal <Alban.SHAHAJ@ext.efsa.europa.eu>
2019-10-16 09:58:56 +0200

    new maven folder structure + licenses

--- src/main/java/sql/SQLStatement.java
+++ src/main/java/sql/SQLStatement.java
@@ -1,26 +1,28 @@
 package sql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class SQLStatement {
 
 	private Connection con;
 	private String query;
-	
+
 	public SQLStatement(Connection con, String query) {
 		this.con = con;
 		this.query = query;
 	}
-	
+
 	public void exec() throws SQLException {
-		try(Statement stmt = con.createStatement();) {
-			stmt.execute(query);
+
+		try (PreparedStatement stmt = con.prepareStatement(query);) {
+			stmt.execute();
 			stmt.close();
 		}
+
 	}
-	
+
 	@Override
 	public String toString() {
 		return query;
